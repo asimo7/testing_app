0234db7409e8c873311c7b6b75b48cba2c4508cb
asimo7 <117193226+asimo7@users.noreply.github.com>
2024-11-29 14:44:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("https://simonteh.pythonanywhere.com");
+const socket = io("https://python-script-dlj2.onrender.com");
 function App() {
   const [warrants, setWarrants] = useState([]);
   const [filteredWarrants, setFilteredWarrants] = useState([]);
